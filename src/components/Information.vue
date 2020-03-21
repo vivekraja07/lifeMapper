99a67275cdfe3c53be47f781cc7fb1da936a8638
vivekraja07 <vivekraja07@gmail.com>
2020-03-21 13:58:44 +0800

    made json into separate file and made arrays of find-index instead of relying on internals of the array staying the same

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -12,15 +12,18 @@
         <section v-if="!propertyName.includes('Languages')">
           <div class = "School" v-if="propertyName=='school' || propertyName=='Education'">
             <div class = "logoHolder">
-                <img :src="value.icon"/>
+               <img :src="require('@/assets/icons/' + value.icon)"/>
             </div>
+
             <div class = "summaryInfo" v-if="value.icon">
                 <h3 class = "title"> {{value.title}} </h3>
                 <h4 class = "name"> {{value.name}} </h4>
                 <h5 class = "timeRange"> {{value.range}} </h5>
                 <h5 v-if="value.location" class = "location"> {{value.location}} </h5>
             </div>
-              <div class = "extraDetails" v-if="value.courses || value.addDetails">
+
+            <div class = "extraDetails" v-if="value.courses || value.addDetails">
+            
               <table v-if="value.courses">
                   <thead>
                       <tr>
@@ -49,7 +52,7 @@
             <h2 class="sectionTitle">Jobs</h2>
             <div class="Job" v-for="(job, index) in value" :key="index">
               <div class = "logoHolder" v-if="job.icon">
-                <img :src="job.icon"/>
+                <img :src="require('@/assets/icons/' + job.icon)"/>
               </div>
               <div class = "summaryInfo" v-if="job.icon">
                 <h3 class = "title"> {{job.title}} </h3>
@@ -87,7 +90,7 @@
               <h2 class="sectionTitle">Activities</h2>
             <div class="Activity" v-for="activity in value" :key="activity.name">
               <div class = "logoHolder">
-                <img :src="activity.icon"/>
+                <img :src="require('@/assets/icons/' + activity.icon)"/>
               </div>
               <div class = "summaryInfo">
                 <h3 class = "title"> {{activity.title}} </h3>
@@ -117,7 +120,8 @@ import { mapActions } from 'vuex'
 export default {
   name: 'Information',
   props: {
-    events: Object
+    events: Object,
+    eventOrder: Array
   },
   data: function () {
     return {
@@ -130,7 +134,6 @@ export default {
       comp_colors: [
         '#1D2951',
         '#003152',
-
         '#111E6C',
         '#000080',
         '#1034A6',
@@ -140,7 +143,6 @@ export default {
         '#6593F5',
         '#3FE0D0',
         '#0F52BA'
-
       ]
     }
   },
@@ -167,8 +169,8 @@ export default {
       'sideBarToggle'
     ]),
     getNext () {
-      var size = Object.keys(this.events).length
-      var index = Object.keys(this.events).indexOf(this.active)
+      var size = Object.keys(this.eventOrder).length
+      var index = this.eventOrder.indexOf(this.active)
       if (index + 1 >= size) {
         console.log('This is still a mystery! Google?')
       } else {
@@ -177,7 +179,8 @@ export default {
       gtag('event','click',{'event_category':'navigation','event_label':'left'});
     },
     getPrev () {
-      var index = Object.keys(this.events).indexOf(this.active)
+      var size = Object.keys(this.eventOrder).length
+      var index = this.eventOrder.indexOf(this.active)
       if (index - 1 < 0) {
         console.log('This is out of scope for this project!')
       } else {
@@ -224,83 +227,90 @@ h3 {
   padding-top: 0px;
 }
 
-    #events {
-      overflow: auto;
-      height: 90%;
-    }
+#events {
+  overflow: auto;
+  height: 90%;
+}
 
-    .sectionTitle {
-        text-align: center;
-    }
+.sectionTitle {
+    text-align: center;
+}
 
-    /* date styles go here */
-    .dates {
-        background: black;
-        color: white;
-        border: 0;
-        height: 10%;
+/* date styles go here */
+.dates {
+    background: black;
+    color: white;
+    border: 0;
+    height: 10%;
 
-        display: flex;
-        justify-content:  space-around;
-        align-content: center;
-        flex-direction: row;
+    display: flex;
+    justify-content:  space-around;
+    align-content: center;
+    flex-direction: row;
 
-        text-align: center;
-    }
+    text-align: center;
+}
 
-    b {
-        font-size: 30px;
-        text-decoration: none;
-    }
+b {
+    font-size: 30px;
+    text-decoration: none;
+}
 
-    b:hover {
-        background-color: #ddd;
-        color: black;
-        cursor: pointer;
-    }
+b:hover {
+    background-color: #ddd;
+    color: black;
+    cursor: pointer;
+}
 
-    /* section styles go here */
+/* section styles go here */
 
-    section {
-        border: 3px solid;
-        border-style: solid;
-        border-top: 0;
-        padding: 20px 72px 16px 24px;
-        text-align: left;
-    }
+section {
+    border: 3px solid;
+    border-style: solid;
+    border-top: 0;
+    padding: 20px 72px 16px 24px;
+    text-align: left;
+}
 
-    /* logo icon styles go here */
+/* logo icon styles go here */
 
-    .logoHolder {
-        width: 72px;
-        height: 72px;
-        float: left;
-    }
+.logoHolder {
+  width: 72px;
+  height: 72px;
+  float: left;
+}
 
-    img {
-        border-radius: 6px;
-        border: 4px solid transparent;
-        height: 64px;
-        width: 64px;
-    }
+.imageHolder {
+  border-radius: 6px;
+  height: 56px;
+  width: 56px;
+  background-size: contain;
+}
 
-    .summaryInfo {
-        margin-left: 80px;
-    }
+img {
+    border-radius: 6px;
+    border: 4px solid transparent;
+    height: 64px;
+    width: 64px;
+}
 
-    .extraDetails {
-        margin-left: 80px;
-        margin-top: 16px;
-    }
+.summaryInfo {
+    margin-left: 80px;
+}
 
-    /* table styles */
-    table {
-        text-align: center;
-        border: 1px solid;
-    }
+.extraDetails {
+    margin-left: 80px;
+    margin-top: 16px;
+}
 
-    th {
-        border-bottom: 1px solid;
-    }
+/* table styles */
+table {
+    text-align: center;
+    border: 1px solid;
+}
+
+th {
+    border-bottom: 1px solid;
+}
 
 </style>
