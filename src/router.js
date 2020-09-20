16c1834064571cfb6ea28479d7670d71906d40a3
vivekraja07 <vivekraja07@gmail.com>
2020-09-20 16:43:19 -0400

    some changes? - not sure what exactly?

--- src/router.js
+++ src/router.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Welcome from './views/Welcome.vue'
+// import Welcome from './views/Welcome.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'welcome',
+    //   component: Welcome
+    // },
     {
       path: '/',
-      name: 'welcome',
-      component: Welcome
-    },
-    {
-      path: '/home',
       name: 'home',
       component: Home
     },
